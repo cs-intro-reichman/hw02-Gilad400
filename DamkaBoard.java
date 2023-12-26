3d723f43fb8aaa9cead3d380d911524712b35e4e
Gilad400 <gilad.shmuel@post.runi.ac.il>
2023-12-26 23:17:46 +0200

    2

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,14 +4,31 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
+		//for(int i = 0; i < n; i++){
+			//if(i % 2 != 0){
+			//	System.out.print(" ");
+			//}
+			//for(int j = 0; j < n; j++){
+			//	System.out.print("* ");
+			//}
+		//System.out.println();
+		//}
+		
+		String stars = "";
 		for(int i = 0; i < n; i++){
-			if(i % 2 != 0){
+			if(i == n-1){
+				stars = stars + "*";
+			} else{
+				stars = stars + "* ";
+			}
+		}
+		for(int j = 0; j < n; j++){
+			if(j % 2 != 0){
 				System.out.print(" ");
 			}
-			for(int j = 0; j < n; j++){
-				System.out.print("* ");
-			}
-		System.out.println();
+			System.out.println(stars);
 		}
+		
+		
 	}
 }
