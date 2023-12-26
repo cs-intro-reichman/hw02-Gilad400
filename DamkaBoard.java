e6e073969fe2572cf558ab551b6366fcc86203de
Gilad400 <gilad.shmuel@post.runi.ac.il>
2023-12-26 22:59:20 +0200

    1

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,11 +5,11 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
 		for(int i = 0; i < n; i++){
-			if(i % 2 == 0){
+			if(i % 2 != 0){
 				System.out.print(" ");
 			}
 			for(int j = 0; j < n; j++){
-				System.out.print("* ");
+				System.out.print(" *");
 			}
 		System.out.println();
 		}
