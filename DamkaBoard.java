0aa44d5369acec3e2269470eebcfa7baa8ba213d
Gilad400 <gilad.shmuel@post.runi.ac.il>
2023-12-27 00:22:12 +0200

    27.12

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,16 +4,6 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
-		//for(int i = 0; i < n; i++){
-			//if(i % 2 != 0){
-			//	System.out.print(" ");
-			//}
-			//for(int j = 0; j < n; j++){
-			//	System.out.print("* ");
-			//}
-		//System.out.println();
-		//}
-		
 		String stars = "";
 		for(int i = 0; i < n; i++){
 			if(i == n-1){
--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -24,6 +24,51 @@ public class OneOfEachStats {
 		//// just like you had in the previous version, except that the 
 		//// randomization will be based on the given seed.
 		//// This is the only change that you have to do in the program.
-		    
+		   
+		String children = "";
+		double totalSum = 0;
+		int two = 0;
+		int three = 0;
+		int four = 0;
+		String mostCommon = "";
+		for(int i = 0; i < T; i++){
+			int sumTemp = 0;
+			while((children.indexOf('g') == -1) || (children.indexOf('b') == -1)){
+				if(generator.nextDouble() < 0.5){
+					children = children + "g ";
+				} else {
+					children = children + "b ";
+				}
+				sumTemp++;
+				totalSum++;
+			}
+			if(sumTemp == 2){
+				two++;
+				if(two > three && two > four){
+					mostCommon = "The most common number of children is 2.";
+				}
+			} 
+			if(sumTemp == 3){
+				three++;
+				if(three > two && three > four){
+					mostCommon = "The most common number of children is 3.";
+				}
+			} 
+			if(sumTemp >= 4){
+				four++;
+				if(four > three && four > two){
+					mostCommon = "The most common number of children is 4 or more.";
+				}
+			}
+			sumTemp = 0;
+			children = "";
+		}
+		double average = (totalSum / T);
+		System.out.println("Average: " + average + " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: " + two);
+		System.out.println("Number of families with 3 children: " + three);
+		System.out.println("Number of families with 4 or more children: " + four);
+		System.out.println(mostCommon);
+	}
 	}
 }
