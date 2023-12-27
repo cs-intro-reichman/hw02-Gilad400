1c696654b618db9902956f2705c46c0ded98fc4e
Gilad400 <gilad.shmuel@post.runi.ac.il>
2023-12-27 17:04:09 +0200

    Change to 'do-hwile' loop

--- InOrder.java
+++ InOrder.java
@@ -6,13 +6,15 @@ public class InOrder {
 	public static void main (String[] args) {
 		//Generate the first random number
 		int random1 = (int)(Math.random() * 10);
-		//Initialize a variable to save the previous random number 
+		//Initialize a variable to save the previous random number
 		int check = 0;
 		//A loop in which we generate random numbers until the privious number is grater then the new one
-		while(check <= random1){
+		do{
 			System.out.print(random1 + " ");
 			check = random1;
 			random1 = (int)(Math.random() * 10);
-		}
+			
+			
+		} while (check <= random1);
 	}
 }
